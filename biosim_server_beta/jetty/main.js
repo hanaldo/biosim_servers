b5b69143147271adb33eae141947c701fa7cc397
Shenshen Han <hanaldo520124@gmail.com>
2018-09-13 12:24:54 -0400

    Update Electron working code.

--- biosim_server_beta/jetty/main.js
+++ biosim_server_beta/jetty/main.js
@@ -1,18 +1,7 @@
 const cp = require("child_process");
+const fs = require("fs");
 
-var javaProcess = cp.exec('java -jar start.jar', (error, stdout, stderr) => {
-  console.log("Never called bye");
-});
-
-javaProcess.stdout.on("data", function(data) {
-  if (data.includes("Thank you! You server is ready")) {
-    console.log("Java server ready!");
-
-    setTimeout(function() {
-      mainWindow.loadURL("http://localhost:8080/index.html");
-    }, 1000);
-  }
-});
+let javaProcess;
 
 process.on("SIGINT", function() {
   console.log("Kill javaProcess");
@@ -24,28 +13,69 @@ let mainWindow;
 
 const {
   app,
-  BrowserWindow
-} = require('electron')
+  BrowserWindow,
+  dialog
+} = require("electron");
 app.commandLine.appendSwitch("disable-http-cache");
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', function() {
-  console.log("createWindow");
-  // Create the browser window.
+app.on("ready", function() {
+  var path = app.getAppPath();
+  //var path = app.getAppPath().replace("default_app.asar", "");
+
   mainWindow = new BrowserWindow({
     width: 1024,
     height: 768
   });
 
+  mainWindow.loadFile("loading.html");
+
   mainWindow.on("closed", function() {
     mainWindow = null
   });
+
+  // mainWindow.on("show", function() {
+  //   dialog.showMessageBox(mainWindow, {
+  //     type: "info",
+  //     buttons: ["OK"],
+  //     title: "App Path",
+  //     message: path
+  //   });
+  // });
+
+  javaProcess = cp.exec("java -jar start.jar", {
+    cwd: app.getAppPath()
+  }, (error, stdout, stderr) => {
+    console.log("Java command finished");
+    // dialog.showMessageBox(mainWindow, {
+    //   type: "info",
+    //   buttons: ["OK"],
+    //   title: "App Path",
+    //   message: "Java command finished"
+    // });
+  });
+
+  javaProcess.stderr.pipe(fs.createWriteStream(path + "/log.txt", {
+    flags: "w"
+  }));
+
+  javaProcess.stdout.on("data", function(data) {
+    if (data.includes("Thank you! You server is ready")) {
+      console.log("Java server ready!");
+      console.log("Wait 1 second to load index page...");
+      setTimeout(function() {
+        mainWindow.loadURL("http://localhost:8080/index.html");
+      }, 1000);
+    }
+  });
+
+  //mainWindow.webContents.openDevTools();
 });
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function() {
+app.on("window-all-closed", function() {
   console.log("Bye BioSim");
   app.quit();
 });
@@ -53,5 +83,4 @@ app.on('window-all-closed', function() {
 app.on("quit", function() {
   console.log("Kill javaProcess");
   javaProcess.kill("SIGINT");
-  javaProcess = null;
 });
