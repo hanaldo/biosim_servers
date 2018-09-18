3ea219a0f313ae0e0b270a54c0ee53df3ca7663e
Shenshen Han <hanaldo520124@gmail.com>
2018-09-18 13:20:23 -0400

    Get all the child process ids of the root javaProcess. Kill all the processes in different platforms.

--- biosim_server_beta/jetty/main.js
+++ biosim_server_beta/jetty/main.js
@@ -1,13 +1,26 @@
 const cp = require("child_process");
 const fs = require("fs");
+const os = require("os");
+const psTree = require("ps-tree");
 
 let javaProcess;
 
-process.on("SIGINT", function() {
+process.on("SIGINT", function() { // for dev
   console.log("Kill javaProcess");
   javaProcess.kill("SIGINT");
   process.exit();
 });
+
+function killProcess(pid) {
+  switch (process.platform) {
+    case "win32":
+      cp.execSync("taskkill /pid " + pid + " /T /F");
+      break;
+    default:
+      cp.execSync("kill -9 " + pid);
+      break;
+  }
+}
 
 let mainWindow;
 
@@ -23,7 +36,8 @@ app.commandLine.appendSwitch("disable-http-cache");
 // Some APIs can only be used after this event occurs.
 app.on("ready", function() {
   var path = app.getAppPath();
-  //var path = app.getAppPath().replace("default_app.asar", "");
+  //var path = app.getAppPath().replace("default_app.asar", "") + "app/";
+  console.log(path);
 
   mainWindow = new BrowserWindow({
     width: 1024,
@@ -37,7 +51,7 @@ app.on("ready", function() {
   });
 
   javaProcess = cp.exec("java -jar start.jar", {
-    cwd: app.getAppPath()
+    cwd: path
   }, (error, stdout, stderr) => {
     console.log("Java command finished");
   });
@@ -45,6 +59,20 @@ app.on("ready", function() {
   // javaProcess.stderr.pipe(fs.createWriteStream(path + "/log.txt", {
   //   flags: "w"
   // }));
+
+  var log = fs.createWriteStream(path + "/log.txt", {
+    flags: "w"
+  });
+  log.write("path: " + path + "\n")
+  log.write("javaProcess.pid: " + javaProcess.pid + "\n");
+  log.write("process.pid: " + process.pid + "\n");
+  log.write("os.platform: " + os.platform + "\n");
+
+  psTree(javaProcess.pid, function(err, children) {
+    children.map(function(p) {
+      log.write("child: " + p.PID + "\n");
+    });
+  });
 
   javaProcess.stdout.on("data", function(data) {
     if (data.includes("Thank you! You server is ready")) {
@@ -61,11 +89,19 @@ app.on("ready", function() {
 
 // Quit when all windows are closed.
 app.on("window-all-closed", function() {
-  console.log("Bye BioSim");
+  console.log("Bye BioSimer");
   app.quit();
 });
 
 app.on("quit", function() {
-  console.log("Kill javaProcess");
-  javaProcess.kill("SIGINT");
+  console.log("Try kill javaProcess");
+  killProcess(javaProcess.pid);
+  //javaProcess.kill("SIGINT");
+  // psTree(javaProcess.pid, function(err, children) {
+  //   children.map(function(p) {
+  //     log.write("kill grandchild: " + p.PID + "\n");
+  //     killProcess(p.PID);
+  //   });
+  // });
+  process.exit();
 });
